290490bb6b0b4bfb1505de78ef5552877a744685
Steve Puthuran <Steve.Puthuran@target.com>
2024-12-04 18:48:14 +0530

    Day 00

--- src/Day00.kt
+++ src/Day00.kt
@@ -10,12 +10,12 @@ fun main() {
     // Test if implementation meets criteria from the description, like:
     check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
+    // Or read a large test input from the `src/Day00_test.txt` file:
+    val testInput = readInput("Day00_test")
     check(part1(testInput) == 1)
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    // Read the input from the `src/Day00.txt` file.
+    val input = readInput("Day00")
     part1(input).println()
     part2(input).println()
 }
